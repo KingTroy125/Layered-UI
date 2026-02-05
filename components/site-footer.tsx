4762a895f7fceb5fa6d53b06ebd72e5a248d0176
Tristan Hendricks <moonknight1355@gmail.com>
2026-02-05 12:49:20 +0200

    Update: minor changes

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -13,20 +13,26 @@ export const SiteFooter = () => (
             <div className="mt-2 flex flex-wrap gap-1 overflow-hidden text-sm font-medium text-gray-700 dark:text-gray-300">
                 Designed by
                 <div className="flex items-start justify-center gap-2 text-center font-semibold text-gray-950 sm:gap-1 dark:text-white">
-                    <div className="block">
-                        <div className="block pb-2">KingTroy125</div>
-                        <div className="-mt-5 grow overflow-hidden">
-                            <svg aria-hidden="true" className="w-16" height="22" viewBox="0 0 283 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1.24715 19.3744C72.4051 10.3594 228.122 -4.71194 281.724 7.12332" stroke="url(#paint0_linear_pl)" strokeWidth="4"></path>
-                                <defs>
-                                    <linearGradient id="paint0_linear_pl" x1="282" y1="5.49999" x2="40" y2="13" gradientUnits="userSpaceOnUse">
-                                        <stop stopColor="#fa1515"></stop>
-                                        <stop offset="1" stopColor="#5560f7"></stop>
-                                    </linearGradient>
-                                </defs>
-                            </svg>
+                    <Link 
+                        href="https://tristanhendricks.vercel.app" 
+                        target="_blank" 
+                        className="hover:text-primary transition-colors duration-200 hover:underline"
+                    >
+                        <div className="block">
+                            <div className="block pb-2">KingTroy125</div>
+                            <div className="-mt-5 grow overflow-hidden">
+                                <svg aria-hidden="true" className="w-16" height="22" viewBox="0 0 283 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1.24715 19.3744C72.4051 10.3594 228.122 -4.71194 281.724 7.12332" stroke="url(#paint0_linear_pl)" strokeWidth="4"></path>
+                                    <defs>
+                                        <linearGradient id="paint0_linear_pl" x1="282" y1="5.49999" x2="40" y2="13" gradientUnits="userSpaceOnUse">
+                                            <stop stopColor="#fa1515"></stop>
+                                            <stop offset="1" stopColor="#5560f7"></stop>
+                                        </linearGradient>
+                                    </defs>
+                                </svg>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 </div>
             </div>
             <div className="flex">
